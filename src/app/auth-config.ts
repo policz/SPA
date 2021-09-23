198bf75ab31832255218d8cff114d5eeaa23341f
Masna <akhilesh.masna@accenture.com>
2021-09-23 16:06:29 -0400

    first commit

---
+++ src/app/auth-config.ts
@@ -0,0 +1,63 @@
+/**
+ * This file contains authentication parameters. Contents of this file
+ * is roughly the same across other MSAL.js libraries. These parameters
+ * are used to initialize Angular and MSAL Angular configurations in
+ * in app.module.ts file.
+ */
+
+import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+/**
+ * Enter here the user flows and custom policies for your B2C application,
+ * To learn more about user flows, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview
+ * To learn more about custom policies, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
+ */
+ export const b2cPolicies = {
+    names: {
+        signUpSignIn: "B2C_1A_SIGNUP_SIGNIN",
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: "https://EDCB2CDev.b2clogin.com/EDCB2CDev.onmicrosoft.com/B2C_1A_SIGNUP_SIGNIN",
+        }
+    },
+    authorityDomain: "EDCB2CDev.b2clogin.com"
+};
+
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ */
+ export const msalConfig: Configuration = {
+    auth: {
+        clientId: '99e6756f-4ec3-488b-ae04-d56416d1dfdb',
+        authority: b2cPolicies.authorities.signUpSignIn.authority, 
+        knownAuthorities: [b2cPolicies.authorityDomain],
+        redirectUri: '/'
+    },
+    cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+        storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(logLevel: LogLevel, message: string) {
+                console.log(message);
+            },
+            logLevel: LogLevel.Verbose,
+            piiLoggingEnabled: false
+        }
+    }
+}
+
+/**
+ * An optional silentRequest object can be used to achieve silent SSO
+ * between applications by providing a "login_hint" property.
+ */
+export const silentRequest = {
+    scopes: ["openid", "profile"],
+    loginHint: "example@domain.net"
+};
